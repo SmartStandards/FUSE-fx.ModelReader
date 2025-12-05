9738a5ba5b0d03a81e7d097d7f51fbcaab91754b
CI Build <ci@ushell.org>
2025-12-05 23:01:52 +0000

    Merge branch 'master' of https://github.com/SmartStandards/FUSE-fx.ModelReader -> VERSIONING [skip ci]

--- dotnet/src/ModelReader.net48/Properties/AssemblyInfo.cs
+++ dotnet/src/ModelReader.net48/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("46adc8b8-2f14-4a31-87ca-95b71ee09ea9")]
 
-[assembly: AssemblyVersion("0.2.16")]
-[assembly: AssemblyInformationalVersion("0.2.16")]
-//DONT ADD: [assembly: AssemblyFileVersion("0.2.16")] !!!
+[assembly: AssemblyVersion("0.2.17")]
+[assembly: AssemblyInformationalVersion("0.2.17")]
+//DONT ADD: [assembly: AssemblyFileVersion("0.2.17")] !!!
